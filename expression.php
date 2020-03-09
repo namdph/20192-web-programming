d1b4ce0555390a74ef0a90739134507525609fec
namdinh <48562211+namdph@users.noreply.github.com>
2020-03-10 02:58:53 +0700

    Create expression.php
    Expression example

---
+++ expression.php
@@ -0,0 +1,16 @@
+<html>
+    <head> 
+        <title>
+            Expression Example
+        </title> 
+    </head>
+    <body>
+        <?php
+        $grade1 = 50;
+        $grade2 = 100;
+        $grade3 = 75;
+        $average = ($grade1 + $grade2 + $grade3) / 3;
+        print ("The average is $average");
+        ?> 
+    </body> 
+</html>
