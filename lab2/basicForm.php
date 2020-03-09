cdea6497603083ec4142cf4bed9fcbb882b295e7
namdinh <48562211+namdph@users.noreply.github.com>
2020-03-10 04:24:44 +0700

    add basicform

---
+++ lab2/basicForm.php
@@ -0,0 +1,12 @@
+<html>
+    <head> <title> A Simple Form </title> </head>
+    <body>
+        <form
+            action="First.php"
+            method="post" >
+            //Click submit to start our initial PHP program.
+            <br> <input type="submit" value="Click To Submit">
+            <input type="reset" value="Erase and Restart">
+        </form>
+    </body> 
+</html>
