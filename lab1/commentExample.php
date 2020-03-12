52cdff06ee25cfded23a3ecb7d8991c1045405ab
namdinh <48562211+namdph@users.noreply.github.com>
2020-03-13 01:53:51 +0700

    add 1.4

---
+++ lab1/commentExample.php
@@ -0,0 +1,18 @@
+<html> 
+    <head> 
+        <title> Generating HTML From PHP</title>
+    </head> 
+    <body> 
+        <h1> Generating HTML From PHP</h1> 
+        <?php
+        // Example script to output HTML tags 
+        print ("Using PHP has <i>some advantages:</i>");
+        print ("<ul>
+                <li>Speed</li>
+                <li>Ease of use</li>
+                <li>Functionality</li>
+                </ul>"); //Output bullet list 
+        print ("</body></html>");
+        ?>
+    </body>
+</html>
